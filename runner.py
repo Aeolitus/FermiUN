2027e2dddb593fd9cfcbd677a498b9d535d8546b
Aeolitus <praiodor@gmail>
2021-12-03 22:38:56 +0100

    Readded original folder, need to fix config

--- runner.py
+++ runner.py
@@ -1,7 +1,7 @@
 from FermiUN import FermiUN
 
-# Already imported: r'Y:\Archiv\2D\Experiment\2021\20210202\Images20210202\Thermo_Remeasure'
-folders = [r'Y:\Archiv\2D\Experiment\2021\20210202\Images20210202\Thermo_Heating', \
+folders = [r'Y:\Archiv\2D\Experiment\2021\20210202\Images20210202\Thermo_Remeasure', \
+    r'Y:\Archiv\2D\Experiment\2021\20210202\Images20210202\Thermo_Heating', \
     r'Y:\Archiv\2D\Experiment\2021\20210127\Images20210127\Thermo_Unitarity', \
     r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_106_Th', \
     r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_107_Th', \
