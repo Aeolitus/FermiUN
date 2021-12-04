fd636faa96e0d13f4ebec94e8ac94bbf46e2d908
Aeolitus <praiodor@gmail.com>
2021-12-03 17:29:09 -0800

    Added inferrence of a single image from hard drive

---
+++ FermiUN/ModelHandler/_infer_image.py
@@ -0,0 +1,18 @@
+import numpy as np
+from skimage.io import imread
+from FermiUN.DataHandler._crop_and_copy_from_folder import read_crop_and_cap
+from FermiUN.ModelHandler._create_batch import mask_image
+
+def infer_image(self, imagepath : str) -> tuple[np.array, np.array, np.array]:
+    '''
+    Given a path to an image, evaluate the model on it
+    :param imagepath: Path to the image file
+    :return: masked image, predicted image, true image
+    '''
+    img = self.read_crop_and_cap(imagepath)
+    X, Y = self.mask_image(img)
+
+    prediction = self.model.predict(X[np.newaxis, :, :])
+    prediction = prediction[0,:,:]
+
+    return X, prediction, Y
