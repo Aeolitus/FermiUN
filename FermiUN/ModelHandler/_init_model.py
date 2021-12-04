ac0b55c44b4fcf3e6cdf8e38ada9d6f302a2ac81
Aeolitus <praiodor@gmail.com>
2021-12-03 16:16:03 -0800

    Decided against initializing from an old model for now

--- FermiUN/ModelHandler/_init_model.py
+++ FermiUN/ModelHandler/_init_model.py
@@ -1,2 +1,11 @@
+from os import listdir
+from os.path import isfile
+
+
 def init_model(self):
+    '''
+    If this folder was already trained on, a model was saved. 
+    This method loads the model to continue where it left off.
+    It is not currently in use.
+    '''
     pass
