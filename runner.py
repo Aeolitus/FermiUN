2897bb606ead5234942b4b259836c904a9edc7fe
Aeolitus <praiodor@gmail>
2021-12-04 22:51:14 +0100

    first images imported, 11k so far

--- runner.py
+++ runner.py
@@ -1,25 +1,26 @@
 from FermiUN import FermiUN
 
-folders = [r'Y:\Archiv\2D\Experiment\2021\20210202\Images20210202\Thermo_Remeasure', \
-    r'Y:\Archiv\2D\Experiment\2021\20210202\Images20210202\Thermo_Heating', \
-    r'Y:\Archiv\2D\Experiment\2021\20210127\Images20210127\Thermo_Unitarity', \
-    r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_106_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_107_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_108_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_109_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_99_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_99_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_102_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_102_Th_Const', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_102_Th_Const_1', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_103_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_104_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_105_Th', \
-    r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_106_Th']
+#folders = [r'Y:\Archiv\2D\Experiment\2021\20210202\Images20210202\Thermo_Remeasure', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210202\Images20210202\Thermo_Heating', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210127\Images20210127\Thermo_Unitarity', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_106_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_107_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_108_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210124\Images20210124\Cal_109_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_99_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_99_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_102_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_102_Th_Const', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_102_Th_Const_1', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_103_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_104_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_105_Th', \
+    #r'Y:\Archiv\2D\Experiment\2021\20210120\Images20210120\Cal_106_Th']
 
-# TODO: Fix the placeholders in the config file, make a real one.
+folders = []
+
 f = FermiUN("./ConfigFiles/testconfig.yaml")
-#f.import_images(folders, "BrightM")
+f.import_images(folders, "BrightM")
 
 #f.initialize_for_training()
 #f.train()
