83d7480a659cf65cc5fa4ff1a317b500d20aa584
Aeolitus <praiodor@gmail.com>
2021-12-03 17:37:32 -0800

    Added plotting of inferrence on single image

--- FermiUN/ModelHandler/_plotting.py
+++ FermiUN/ModelHandler/_plotting.py
@@ -20,3 +20,30 @@ def plot_loss_curve(self):
 
     plt.show(block=False)
     plt.pause(0.001)
+
+def plot_inferrence(self, masked : np.array, inferred : np.array, truth : np.array):
+    '''
+    Plots the result of an inferrence on a single image.
+    :param masked: Masked input to the model
+    :param inferred: Model output
+    :param truth: Ground truth
+    '''
+    fig = plt.figure(2)
+    plt.clf()
+    fig.set_facecolor('white')
+
+    plt.subplot(1,4,1)
+    plt.imshow(masked, vmin=0.0, vmax=0.9)
+    plt.title("Masked Input")
+
+    plt.subplot(1,4,2)
+    plt.imshow(truth, vmin=0.0, vmax=0.9)
+    plt.title("Target")
+
+    plt.subplot(1,4,3)
+    plt.imshow(inferred, vmin=0.0, vmax=0.9)
+    plt.title("Model Result")
+
+    plt.subplot(1,4,4)
+    plt.imshow(truth-inferred, vmin=0.0, vmax=0.9)
+    plt.title("Difference")
