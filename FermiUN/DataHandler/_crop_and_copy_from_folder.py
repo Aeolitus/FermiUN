abf5e131f9fa5733a0736af78ce89a9e27836871
Aeolitus <praiodor@gmail.com>
2021-12-03 17:11:44 -0800

    Separated load crop and cap into method to load from elsewhere

--- FermiUN/DataHandler/_crop_and_copy_from_folder.py
+++ FermiUN/DataHandler/_crop_and_copy_from_folder.py
@@ -4,6 +4,26 @@ import numpy as np
 from skimage.io import imread
 from tqdm import tqdm
 
+def read_crop_and_cap(self, imagepath : str) -> np.array:
+    top = int(self.config.image.center_y-self.config.image.width/2.0)
+    left = int(self.config.image.center_x-self.config.image.width/2.0)
+    if top < 0 or left < 0:
+        raise Exception("Crop window exceeds image bounds.")
+
+    img = imread(imagepath)
+    img = img[top : top + self.config.image.width, \
+        left : left + self.config.image.width]
+
+    # Rescale and cut of outliers
+    # TODO: This should probably be automatically determined from the images
+    img = img / self.config.image.max_value
+    img[img > 1.0] = 1.0 # TODO: Should this be somehow marked as an outlier?
+    img[img < 0.0] = 0
+
+    return img
+
+    
+
 def crop_and_copy_from_folder(self, folderpath : str, imagename : str):
     '''
     Takes all images in a folder where the filename contains a certain word,
@@ -19,26 +39,14 @@ def crop_and_copy_from_folder(self, folderpath : str, imagename : str):
     if not exists(target_folder):
         mkdir(target_folder)
 
-    top = int(self.config.image.center_y-self.config.image.width/2.0)
-    left = int(self.config.image.center_x-self.config.image.width/2.0)
-    if top < 0 or left < 0:
-        raise Exception("Crop window exceeds image bounds.")
-
     img_list = [join(folderpath, el) for el in listdir(folderpath) if imagename in el]
     old_img_number = len(listdir(target_folder))
     img_number = old_img_number
+    
     for img in tqdm(img_list):
-        img = imread(img)
-        img = img[top : top + self.config.image.width, \
-            left : left + self.config.image.width]
-
-        # Rescale and cut of outliers
-        # TODO: This should probably be automatically determined from the images
-        img = img / self.config.image.max_value
-        img[img > 1.0] = 1.0 # TODO: Should this be somehow marked as an outlier?
-        img[img < 0.0] = 0
-
+        img = read_crop_and_cap(img)
         target_name = target_folder + '/' + self.config.imageprefix + str(img_number)
         np.save(target_name, img)
         img_number = img_number + 1
+    
     print(f"{img_number-old_img_number} images were imported, for a total of {img_number}.")
