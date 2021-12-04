5ea377147293ee03a4d36b47e64991ab10c1b807
Aeolitus <praiodor@gmail.com>
2021-12-03 17:20:35 -0800

    Separated masking into its own method

--- FermiUN/ModelHandler/_create_batch.py
+++ FermiUN/ModelHandler/_create_batch.py
@@ -1,6 +1,23 @@
 import numpy as np
 from skimage.io import imread
 
+def mask_image(self, image : np.array) -> tuple[np.array]:
+    '''
+    Takes an image and returns it masked as well as the original
+    :param image: Input image
+    :returns: Masked image, Original Image
+    '''
+
+    left = int(self.config.image.width / 2 - self.config.radius)
+    right = int(self.config.image.width / 2 + self.config.radius)
+
+    Y = image[left:right, left:right] # what the network should ideally predict
+    temp = image * self.mask
+    X = temp + (1 - self.mask) * 0.5 # set image to 0.5 where the mask is
+
+    return X, Y
+
+
 def create_batch(self, list_of_files : list[str], list_of_indices : list[int]) -> tuple[np.array, np.array]:
     '''
     Creates a batch of masked images and associated targets.
@@ -10,13 +27,8 @@ def create_batch(self, list_of_files : list[str], list_of_indices : list[int]) -
     X = np.empty([len(list_of_indices), self.config.image.width, self.config.image.width])
     Y = np.empty([len(list_of_indices), self.config.radius*2, self.config.radius*2])
 
-    left = int(self.config.image.width / 2 - self.config.radius)
-    right = int(self.config.image.width / 2 + self.config.radius)
-
     for i in range(len(list_of_indices)):
         img = np.load(list_of_files[i])
-        Y[i,:,:] = img[left:right, left:right] # what the network should ideally predict
-        temp = img * self.mask
-        X[i,:,:] = temp + (1 - self.mask) * 0.5 # set image to 0.5 where the mask is
+        X[i,:,:], Y[i,:,:] = self.mask_image(img)
 
     return X, Y
