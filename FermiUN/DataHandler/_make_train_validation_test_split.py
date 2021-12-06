15d89f7888b588af767b08d4289e7e6cdfe1928d
Aeolitus <praiodor@gmail>
2021-12-06 06:48:47 +0100

    small bugfix

--- FermiUN/DataHandler/_make_train_validation_test_split.py
+++ FermiUN/DataHandler/_make_train_validation_test_split.py
@@ -12,11 +12,11 @@ def make_train_validation_test_split(self):
 
     indices = np.arange(len(imagefolder_contents))
     np.random.shuffle(indices)
-    
+    indices = indices.tolist()
+
     index_test_end = int(self.config.test_fraction * len(imagefolder_contents))
     index_validation_end = index_test_end + int(self.config.validation_fraction * len(imagefolder_contents))
 
-    self.config.testlist = indices[0:index_test_end]
-    self.config.validationlist = indices[index_test_end+1:index_validation_end]
-    self.config.trainlist = indices[index_validation_end+1:]
-    
+    self.config.testlist = [imagefolder_contents[i] for i in indices[:index_test_end]]
+    self.config.validationlist = [imagefolder_contents[i] for i in indices[index_test_end+1:index_validation_end]]
+    self.config.trainlist = [imagefolder_contents[i] for i in indices[index_validation_end+1:]]    
